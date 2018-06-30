a9d520d02b3c2203bb839f8aea076e82e1ac5555
Alex Tye <mrlexy@hotmail.com>
2018-06-30 19:31:26 +0100

    Add project files.

---
+++ Delete/Case and Switch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AreYouBill
+    {
+    class AreYouBill
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.Write(name+ " is your name huh? ");
+      
+            switch (name)
+            {
+                    case "bill":
+                    Console.WriteLine("Bill");
+                    break;
+
+                    case "alex":
+                    Console.WriteLine("Alex");
+                    break;
+                    case "tom":
+                    Console.WriteLine("Tom");
+                    break;
+                    default:
+                    Console.WriteLine("I dont know who you are");
+                    break;
+
+
+            }
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
